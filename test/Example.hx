1dbaac5aaa841c7b28a8123819a4d6f9496b0b32
Philipp Klose <me@thehippo.de>
2011-05-06 17:41:10 +0200

    added Example from haXe wiki page

---
+++ test/Example.hx
@@ -0,0 +1,18 @@
+#if neko
+import neko.Web;
+import neko.Lib;
+#else
+import cpp.Web;
+import cpp.Lib;
+#end
+
+class Example {
+
+	static function main()
+		Web.cacheModule(run)
+	
+	static function run() {
+		Web.setHeader("Content-type","text/plain");
+		Lib.println(Web.getParams());
+	}
+}
